88053c8e6bc157eb32bc6072e84e99766ed085ae
padmapriyas-1510 <padmapriyas@isportz.co>
2026-07-27 14:00:58 +0530

    Color code changes

--- dashboard/components/src/utils/colorUtils.js
+++ dashboard/components/src/utils/colorUtils.js
@@ -1,4 +1,6 @@
 export const getStatusColor = (status, category) => {
+
+  console.log("Status:", status, "Category:", category);
   const name = status.toLowerCase();
 
   if (name.includes("cancel")) {
@@ -17,17 +19,28 @@ export const getStatusColor = (status, category) => {
     return "#90EE90";
   }
 
-  if (category === "In Progress") {
-    return "#FF8C00";
+   if (name.includes("client uat")) {
+    return "#9C27B0";      // Purple
   }
 
-  if (name.includes("in review")) {
-    return "#B5E61D";
+  if (name.includes("internal qa")) {
+    return "#2196F3";      // Blue
   }
 
+  if (name.includes("waiting for customer")) {
+    return "#E91E63";      // Pink
+  }
+
+  if (name.includes("in progress")) {
+    return "#FF9800";      // Orange
+  }
+
+  //  if (category === "In Progress") {
+  //   return "#FF8C00";
+  // }
   if (category === "To Do") {
     return "#0052CC";
   }
-
+ 
   return "#A5ADBA";
 };
